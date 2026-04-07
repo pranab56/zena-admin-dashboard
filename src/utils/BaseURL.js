8cc347b93984049b0475fc144b25602f2209e037
pranab <pronabhalder53@gmail.com>
2026-04-07 16:18:19 +0600

    base url changed

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,2 +1,3 @@
-export const baseURL = "https://ismail4000.binarybards.online";
+// export const baseURL = "https://ismail4000.binarybards.online";
+export const baseURL = "https://salonverse-production.up.railway.app";
 // export const baseURL = "https://api.optimushs.com";
